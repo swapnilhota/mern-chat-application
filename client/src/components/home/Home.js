b266d46e4ed11bdedde15eefe9dcf30f7273b668
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-25 11:21:12 +0530

    update home

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const { user, setUser } = useContext(UserContext);
+
     const setAsJohn = () => {
         const john = {
             name: 'John',
@@ -13,9 +15,24 @@ const Home = () => {
         setUser(john);
     }
 
+    const setAsTom = () => {
+        const tom = {
+            name: 'Tom',
+            email: 'tom@email.com',
+            password: '456',
+            id: '456'
+        }
+        setUser(tom);
+    }
+
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home {JSON.stringify(user)}</h1>
+            <button onClick={setAsJohn} >Set as John</button>
+            <button onClick={setAsTom} >Set as Tom</button>
+            <Link to={'/chat'}>
+                <button>Go to Chat</button>
+            </Link>
         </div>
     )
 }
