201fc23f3569f4ad9cb806b7e8ef6a09a4f80a4c
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-25 11:17:08 +0530

    working on user context

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 const Home = () => {
+    const { user, setUser } = useContext(UserContext);
+    const setAsJohn = () => {
+        const john = {
+            name: 'John',
+            email: 'john@email.com',
+            password: '123',
+            id: '123'
+        }
+        setUser(john);
+    }
+
     return (
         <div>
             <h1>Home</h1>
