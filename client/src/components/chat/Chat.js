2d1e274436b27d9f67c7676392655b4918d8b579
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-25 11:25:17 +0530

    userContext done

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../UserContext'
+import { Link } from 'react-router-dom';
 
 const Chat = () => {
+
+    const { user, setUser } = useContext(UserContext);
+
     return (
         <div>
-            <h1>Chat</h1>
+            <h1>Chat {JSON.stringify(user)}</h1>
+            <Link to={'/'}>
+                <button>Go to Home</button>
+            </Link>
         </div>
     )
 }
