ecf9bbcf77a6f2cdf10ca2e11d5251b4b1432fb3
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-26 09:30:31 +0530

    update

--- client/src/components/home/Room.js
+++ client/src/components/home/Room.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
-const Room = () => {
+const Room = ({ name }) => {
     return (
         <div>
-            <div className="col s12 m7">
-                <h2 className="header">Horizontal Card</h2>
-                <div className="card horizontal">
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>I am a very simple card. I am good at containing small bits of information.</p>
-                        </div>
+            <div className="card horizontal">
+                <div className="card-stacked">
+                    <div className="card-content">
+                        <p>{name}</p>
                     </div>
                 </div>
             </div>
