09fd698210ca2466eeffcbe103647432d6ff0619
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-26 10:51:18 +0530

    update server

--- server/index.js
+++ server/index.js
@@ -3,11 +3,25 @@ const http = require('http').createServer(app);
 const socketio = require('socket.io');
 const io = socketio(http);
 const PORT = process.env.PORT || 5000;
+const { addUser } = require('./helper');
 
 io.on('connection', (socket) => {
     console.log(socket.id);
     socket.on('create-room', name => {
         console.log('The room name received is ', name);
+    });
+    socket.on('join', ({ name, room_id, user_id }) => {
+        const { error, user } = addUser({
+            socket_id: socket.id,
+            name: name,
+            room_id: room_id,
+            user_id: user_id
+        });
+        if (error) {
+            console.log('join error ', error);
+        } else {
+            console.log('join user ', user);
+        }
     })
 });
 
