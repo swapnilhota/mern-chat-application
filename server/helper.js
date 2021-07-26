076b1c01115a45024f955fe3f93101189f70eb76
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-26 10:46:55 +0530

    helper in server

---
+++ server/helper.js
@@ -0,0 +1,13 @@
+const users = [];
+
+const addUser = ({ socket_id, name, user_id, room_id }) => {
+    const exist = users.find(user => user.room_id === room_id && user.user_id === user_id);
+    if (exist) {
+        return { error: 'User already exists in this room' };
+    }
+    const user = { socket_id, name, user_id, room_id };
+    users.push(user);
+    return { user };
+}
+
+module.exports = { addUser };
