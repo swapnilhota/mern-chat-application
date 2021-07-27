a8e726a68aa0f9329c8b51471a98d55e2ec52c0e
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-27 10:56:20 +0530

    receive message in server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http').createServer(app);
 const socketio = require('socket.io');
 const io = socketio(http);
 const PORT = process.env.PORT || 5000;
-const { addUser } = require('./helper');
+const { addUser, getUser, removeUser } = require('./helper');
 
 io.on('connection', (socket) => {
     console.log(socket.id);
@@ -22,6 +22,21 @@ io.on('connection', (socket) => {
         } else {
             console.log('join user ', user);
         }
+    });
+    socket.on('sendMessage', (message, room_id, callback) => {
+        const user = getUser(socket.id);
+        const msgToStore = {
+            name: user.name,
+            user_id: user.user_id,
+            room_id: room_id,
+            text: message
+        };
+        console.log('message, received', msgToStore);
+        io.to(room_id).emit('message', msgToStore);
+        callback();
+    });
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
     })
 });
 
