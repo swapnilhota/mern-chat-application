3d3d8983e977907efafc8f0f5817fdbd64ac646e
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-28 16:38:17 +0530

    update

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -25,6 +25,13 @@ const Chat = () => {
         })
     }, [messages])
 
+    useEffect(() => {
+        socket.emit('get-messages-history', room_id);
+        socket.on('output-messages', messages => {
+            setMessages(messages);
+        })
+    }, [])
+
     const sendMessage = event => {
         event.preventDefault();
         if (message) {
