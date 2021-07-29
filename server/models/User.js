f9669c5501163b18cdaf8af602295ddf3ef4c85d
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-29 15:32:21 +0530

    custom error messages

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Please enter a name']
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email address']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'The password should be atleast 6 character long']
     }
 });
 
