f7049f6e4c145baf6c1fccc83fbeedc3cbc4e9aa
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-29 15:05:59 +0530

    server routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+
+router.post('/signup', () => {
+
+})
+
+router.post('/login', () => {
+
+})
+
+router.get('/logout', () => {
+
+})
+
+module.exports = router;
