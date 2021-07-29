56e3b10f75ab8aaf5cec480aae9b1ff5fbf6fa2c
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-29 16:39:39 +0530

    signup component

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
     return (
         <div>
             <div className="row">
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="name" type="text" className="validate" />
+                            <input value={name} onChange={e => setName(e.target.value)} id="name" type="text" className="validate" />
                             <label htmlFor="name">Name</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="email" type="email" className="validate" />
+                            <input value={email} onChange={e => setEmail(e.target.value)} id="email" type="email" className="validate" />
                             <label htmlFor="email">Email</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="password" type="password" className="validate" />
+                            <input value={password} onChange={e => setPassword(e.target.value)} id="password" type="password" className="validate" />
                             <label htmlFor="password">Password</label>
                         </div>
                     </div>
