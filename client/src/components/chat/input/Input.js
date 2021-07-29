518c52cfb68ed054d56c4f497ef38040dab20de0
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-29 11:54:45 +0530

    update

--- client/src/components/chat/input/Input.js
+++ client/src/components/chat/input/Input.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({ message, setMessage, sendMessage }) => {
     return (
         <div>
-
+            <form action="" onSubmit={sendMessage} className="form" >
+                <input
+                    type="text"
+                    className="input"
+                    placeholder="Type a message"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                    onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+                />
+                <button className="sendButton" >Send Message</button>
+            </form>
         </div>
     )
 }
