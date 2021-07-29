b53c0cd7b449195947bbf342c38fe569a91e4c1c
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-29 15:09:57 +0530

    controllers

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,12 @@
 const { Router } = require('express');
+const authController = require('../controllers/authControllers');
+
 const router = Router();
 
-router.post('/signup', () => {
+router.post('/signup', authController.signup);
 
-})
+router.post('/login', authController.login);
 
-router.post('/login', () => {
-
-})
-
-router.get('/logout', () => {
-
-})
+router.get('/logout', authController.logout);
 
 module.exports = router;
