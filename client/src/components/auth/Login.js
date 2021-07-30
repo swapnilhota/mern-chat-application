9d85bfedca04f4d26fd6976bc7db0a29db5a0166
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-30 19:24:47 +0530

    working on login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { UserContext } from '../../UserContext';
+import { Redirect } from 'react-router-dom';
 
 const Login = () => {
+
+    const { user, setUser } = useContext(UserContext);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        setEmailError('');
+        setNameError('');
+        setPasswordError('');
+
+        try {
+            const res = await fetch('http://localhost:5000/login', {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify({ name, email, password }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const data = await res.json();
+            console.log(data);
+            if (data.errors) {
+                setEmailError(data.errors.email);
+                setNameError(data.errors.name);
+                setPasswordError(data.errors.password);
+            }
+            if (data.user) {
+                setUser(data.user);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    if (user) {
+        return <Redirect to="/" />
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="row">
+            <form className="col s12" onSubmit={submitHandler} >
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input value={name} onChange={e => setName(e.target.value)} id="name" type="text" className="validate" />
+                        <div className="name error red-text" >{nameError}</div>
+                        <label htmlFor="name">Name</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input value={email} onChange={e => setEmail(e.target.value)} id="email" type="email" className="validate" />
+                        <div className="email error red-text" >{emailError}</div>
+                        <label htmlFor="email">Email</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input value={password} onChange={e => setPassword(e.target.value)} id="password" type="password" className="validate" />
+                        <div className="password error red-text" >{passwordError}</div>
+                        <label htmlFor="password">Password</label>
+                    </div>
+                </div>
+                <button className="btn" >Sign Up</button>
+            </form>
         </div>
     )
 }
