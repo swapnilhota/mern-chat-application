64d3ceee35e269187917f0f330f130226c7ac623
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-30 19:38:05 +0530

    update login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -50,13 +50,6 @@ const Login = () => {
             <form className="col s12" onSubmit={submitHandler} >
                 <div className="row">
                     <div className="input-field col s12">
-                        <input value={name} onChange={e => setName(e.target.value)} id="name" type="text" className="validate" />
-                        <div className="name error red-text" >{nameError}</div>
-                        <label htmlFor="name">Name</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="input-field col s12">
                         <input value={email} onChange={e => setEmail(e.target.value)} id="email" type="email" className="validate" />
                         <div className="email error red-text" >{emailError}</div>
                         <label htmlFor="email">Email</label>
