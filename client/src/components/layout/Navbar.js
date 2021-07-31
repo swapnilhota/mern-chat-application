17ddfba832d7de93b365cec8098af1874b4accd2
Swapnil Hota <swapnilhota99@gmail.com>
2021-07-31 11:06:03 +0530

    logout functionality

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../UserContext'
 
 const Navbar = () => {
+
+    const { user, setUser } = useContext(UserContext);
+
+    const logout = async () => {
+        try {
+            const res = await fetch('http://localhost:5000/logout', {
+                credentials: true
+            });
+            const data = res.json();
+            console.log('logout data', data);
+            setUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div>
             <nav className="green">
@@ -10,14 +27,14 @@ const Navbar = () => {
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="/login">Login</a></li>
                         <li><a href="/signup">SignUp</a></li>
-                        <li><a href="#">Logout</a></li>
+                        <li onClick={logout} ><a href="#">Logout</a></li>
                     </ul>
                 </div>
             </nav>
             <ul className="sidenav" id="mobile-demo">
                 <li><a href="/login">Login</a></li>
                 <li><a href="/signup">SignUp</a></li>
-                <li><a href="#">Logout</a></li>
+                <li onClick={logout} ><a href="#">Logout</a></li>
             </ul>
         </div>
 
